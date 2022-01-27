4526dc0250add4d79d3770ddd0ab7a37e3a58f67
BaeSungMin <qo98333@naver.com>
2022-01-27 19:27:59 +0900

    FC

---
+++ gulpfile.babel.js
@@ -0,0 +1,44 @@
+import gulp from "gulp";
+import del from "del";
+import sass from "gulp-sass";
+import minify from "gulp-csso";
+import autoprefixer from "gulp-autoprefixer";
+
+sass.compiler = require("node-sass");
+
+const routes = {
+  css: {
+    watch: "./c1/scss/*",
+    src: "./c1/scss/styles.scss",
+    dest: "./c1/css"
+  }
+};
+
+const styles = () =>
+  gulp
+    .src(routes.css.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        flexbox: true,
+        grid: "autoplace"
+      })
+    )
+    .pipe(minify())
+    .pipe(gulp.dest(routes.css.dest));
+
+const watch = () => {
+  gulp.watch(routes.css.watch, styles);
+};
+
+const clean = () => del(["./c1/css"]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([styles]);
+
+const live = gulp.parallel([watch]);
+
+export const dev = gulp.series([prepare, assets, live]);
+//export const dev = gulp.series([assets, live]);
+
